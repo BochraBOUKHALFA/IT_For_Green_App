ee81169d6382f913c20786541bf665515e599569
Jubadj <92817484+Jubadj@users.noreply.github.com>
2023-10-27 13:22:30 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
-npm install -g n
-n latest
+sudo apt-get update
+sudo apt-get install -y curl
+
+# Téléchargement et exécution du script d'installation de NodeSource pour LTS
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
 node -v
 npm install
 npm start
